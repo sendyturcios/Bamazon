f92471af4a97fa4971607ffbb98da4a64db29a33
sturcios <sen.turcios@gmail.com>
2018-07-26 01:47:29 -0500

    added more database info

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -18,5 +18,5 @@ const connection = mysql.createConnection({
   
   connection.connect(function(err) {
     if (err) throw err;
-    
+    console.log("connected as id " + connection.threadId);
   });
